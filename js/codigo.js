9cec319098352bb500e6a9b906f0c393872c7215
Alan Martinez <alannmartinezz@icloud.com>
2022-01-23 11:33:45 -0300

    Se agrega JS y estilos.

--- js/codigo.js
+++ js/codigo.js
@@ -1,35 +1,63 @@
 
-// Evento para mostrar y ocultar menu mobile al hacer "click";
+const menuBtn = document.querySelector(".menu-btn");
+const list = document.querySelector(".list");
+const title = document.querySelector(".title");
+const carousel = document.querySelector(".carousel")
 
-let btnMenu = document.getElementById("btnMenu");
-let menu = document.getElementById("menu");
-let body = document.getElementById("body");
 
-btnMenu.addEventListener("click", function(){
-    
-    menu.classList.toggle("mostrar");
-    body.classList.toggle("noScroll");
-    
-});
+addEventListener("load", ()=>{
+    title.classList.add("visible");
+    // Se agrega la clase "visible" al titulo de la pagina;
+    carousel.classList.add("visible");
+    // Se agrega la clase "visible" al carousel principal;
+})
 
-// Evento para mostrar img al hacer "scroll";
 
-window.addEventListener("scroll", function(){
-    let animacion = document.getElementById("animar");
-    let position = animacion.getBoundingClientRect().top;
-
-    console.log(position);
-
-    
-    let ventana = window.innerHeight;
-
-    console.log(ventana);
-
-        if (position <= 290){
-            animacion.classList.add("aparece");
+const mostrar = ()=>{
+        const clase = menuBtn.firstElementChild.getAttribute("class");
+        // Se obtiene el contenido del atributo class;
+        list.classList.toggle("visible");
+        if(clase == "fas fa-bars"){
+            menuBtn.firstElementChild.setAttribute("class", "fas fa-times");
         }
         else{
-            animacion.classList.remove("aparece");
+            menuBtn.firstElementChild.removeAttribute("class", "fas fa-times");
+            menuBtn.firstElementChild.setAttribute("class", "fas fa-bars");
         }
+    }
 
-});
+// Simulador Iteractivo
+
+const financiacion = (precioViaje)=>{
+    const  confirmacion = confirm(`¿Deseas comenzar a financiar tu viaje?`);
+
+    if(confirmacion){
+        let valorCuota = 0;
+        let cuotas = parseInt(prompt("¿ En cuantas cuotas deseas abonar tu viaje ?"));
+            // Dependiendo el numero de cuotas elegidas se realiza el respectivo calculo.
+            switch(cuotas){
+                case 3:
+                    valorCuota = precioViaje / cuotas;
+                    swal(`El monto abonar es de $${valorCuota}`);
+                    break;
+                case 6:
+                    valorCuota = precioViaje / cuotas;
+                    swal(`El monto abonar es de $${valorCuota}`);
+                    break;
+                case 9:
+                    valorCuota = precioViaje / cuotas;
+                    swal(`El monto abonar es de ${valorCuota}`);
+                    break;
+                case 12:
+                    valorCuota = precioViaje / cuotas;
+                    swal(`El monto abonar es de $${valorCuota}`);
+                    break;
+                default:
+                swal(`El monto abonnar es de $${precioViaje}`);
+                break;
+            }
+    }
+    else{
+        swal("Te esperamos pronto.")
+    }
+}
