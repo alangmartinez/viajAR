6c39ee445be78cec5dcd465aef5e503c9ebd09eb
Alan Martinez <alannmartinezz@icloud.com>
2022-02-17 16:15:51 -0300

    Update form and popup

--- js/codigo.js
+++ js/codigo.js
@@ -12,19 +12,28 @@ btnGuardar = document.getElementById("btn-guardar"),
 btnMostrar = document.getElementById("btn-mostrar"),
 textInput = document.getElementById("text-input"),
 closePopupBtn = document.getElementById("close-popup-btn"),
-popup = document.getElementById("popup");
+popup = document.getElementById("popup"),
+form = document.getElementById("form");
 
-
-addEventListener("load", ()=>{
+// Se ejecuta el evento cuando la ventana esta cargando.
+window.addEventListener("load", ()=>{
+    // Scripting
     title.classList.add("visible");
-    // Se agrega la clase "visible" al titulo de la pagina;
     carousel.classList.add("visible");
-    // Se agrega la clase "visible" al carousel principal;
 });
+
+class Trips {
+    constructor(destination, price, fees, name, mail){
+        this.destination = destination;
+        this.price = Number(price);
+        this.fees = Number(fees);
+        this.name = name;
+        this.mail = mail
+        }
+}
 
 const mostrar = ()=>{
         const clase = menuBtn.firstElementChild.getAttribute("class");
-        // Se obtiene el contenido del atributo class;
         list.classList.toggle("visible");
         if(clase == "fas fa-bars"){
             menuBtn.firstElementChild.setAttribute("class", "fas fa-times");
@@ -38,45 +47,43 @@ const mostrar = ()=>{
 const financiacion = (precioViaje, viaje)=>{
     destination.value = viaje;
     price.value = precioViaje;
-// Validacion de datos
-}
-class Trips {
-    constructor(destination, price, fees, name, mail){
-        this.destination = destination;
-        this.price = Number(price);
-        this.fees = Number(fees);
-        this.name = name;
-        this.mail = mail
-        }
 }
 
-let tripsArray = []; // Se declara un array vacio para almacenar objetos.
+const respuesta = document.createElement("p");
 
 boton.addEventListener("click", (e)=>{
+    // Scripting
     e.preventDefault();
-    let error = validarDatos();
+    let error = validarFormulario(); 
     if(error[0]){
-        document.querySelector(".respuesta").innerHTML = error[1];
-        document.querySelector(".respuesta").classList.add("red");
-        document.querySelector(".respuesta").classList.remove("green");
+        respuesta.textContent = error[1];
+        respuesta.classList.add("red");
+        respuesta.classList.remove("green");
+        form.appendChild(respuesta);
     }
     else{
-        let trip = new Trips(
-            destination.value,
-            price.value,
-            fees.value,
-            userName.value,
-            email.value
-            );
-            document.querySelector(".respuesta").innerHTML = error[1];
-            document.querySelector(".respuesta").classList.add("green");
-            document.querySelector(".respuesta").classList.remove("red");
-            document.getElementById("tripData").innerHTML = `- Destination: ${trip.destination} </br> - Price: ${trip.price} </br> - Fees: ${trip.fees} </br> - Name: ${trip.name} </br> - E-mail: ${trip.mail}`;
-            popup.classList.add("aparecer");
+        var trip = new Trips(
+        destination.value,
+        price.value,
+        fees.value,
+        userName.value,
+        email.value
+        );
+
+        respuesta.textContent = error[1];
+        respuesta.classList.add("green");
+        respuesta.classList.remove("red");
+        form.appendChild(respuesta);
+        popup.classList.add("aparecer");
     }
 });
-const validarDatos = ()=>{
+const validarFormulario = ()=>{
     let error = [];
+    if(destination.value == ""){
+        error[0] = true;
+        error[1] = "* Debe seleccionar un destino";
+        return error;
+    }
     if(fees.value == ""){
         error[0] = true;
         error[1] = "* Cantidad cuotas invalido.";
@@ -104,185 +111,23 @@ const validarDatos = ()=>{
     error[1] = "Solicitud enviada correctamente.";
     return error;
 }
+
+const verDatosBtn = document.createElement("button");
+verDatosBtn.textContent = "Ver Datos";
+verDatosBtn.classList.add("button");
+popup.appendChild(verDatosBtn);
+
+const datos = document.createElement("p");
+
+// Evento click en button "Ver Datos"
+verDatosBtn.addEventListener("click", ()=> {
+    //Scripting
+    datos.textContent = `Destino: ${destination.value}.
+    Precio: ${price.value}.
+    Cuotas: ${fees.value}.
+    Nombre: ${userName.value}.
+    E-mail: ${email.value}`
+    popup.appendChild(datos);
+})
+
 closePopupBtn.addEventListener("click", ()=> popup.classList.remove("aparecer"));
-
-const numbers = [1, 5, 112, 234, 543, 123, 45, 788];
-
-const impares = (arr)=>{ // Declaramos la funcion;
-    const nuevoArray = arr.filter(number => number % 2 != 0); // Filtramos los numberos impares del array;
-    console.log(nuevoArray); // Imprimimos en consola el nuevoArray de numero impares;
-}
-
-impares(numbers);  // Llamamos la funcion; 
-
-const paresEImpares = (arr)=>{
-    let pares = [];
-    let impares = [];
-    arr.map(number => {
-        if(number % 2 == 0){
-            pares.push(number);
-        }
-        else{
-            impares.push(number);
-        }
-    })
-    console.log(`Los numeros pares son: ${pares}`);
-    console.log(`Los numeros impares son: ${impares}`);
-}
-
-paresEImpares(numbers);
-console.log(numbers.slice(start = 0, end = 5));
-
-
-// const armarMenuDesktop = confirm("Deseas armar un menu para desktop?");
-// if(armarMenuDesktop){
-//     alert("Este menu solo sera visible para un dispositivo mayor a 1280px de ancho.");
-//     if(window.innerWidth >= 1280){
-//         const desktopMenu = document.getElementById("desktop-menu");
-//         const cantItems = prompt("Cuantos items quieres que tenga el menu?");
-//         for(let index = 0; index < cantItems; index++){
-//             const listItem = document.createElement("LI");
-//             listItem.innerHTML = prompt(`Ingresa valor item ${index + 1}`);
-//             desktopMenu.appendChild(listItem);
-//             listItem.setAttribute("class","list-item");
-//         }
-//     }
-//     else{
-//         alert("Usted no dispone de un dispositivo con un ancho mayor o igual a 1280px");
-//     }
-// }
-// else{
-//     alert("Goodbye!");
-// }
-
-// for(let index = 0; index < 2; index++){
-//     let tienda = new Tienda(
-//         this.nombre = prompt("Ingrese nombre de la tienda"),
-//         this.direccion = prompt("Ingrese direccion de la tienda."),
-//         this.propietario = prompt("Nombre del propietario."),
-//         this.rubro = prompt("Ingrese rubro")
-//     )
-//     tiendas += `Nombre Tienda: ${this.nombre}
-//     Direccion: ${this.direccion}
-//     Propietario: ${this.propietario}
-//     Rubro: ${this.rubro}\n`
-// }
-// console.log(tiendas);
-
-// const numericArray = [1,45,765,234,6,999];
-
-// let len = numericArray.length;
-// let max = -Infinity;
-// for(let index = 0; index < len; index++){
-//     if(numericArray[index] > max){
-//         max = numericArray[index];
-//     }
-// }
-// console.log(max);
-
-// Actividad 2 (Ejercicio 4)
-
-// class Tienda{
-//     constructor(nombre, direccion, propietario, rubro){
-//         // Propiedades del objeto;
-//         this.nombre = nombre;
-//         this.direccion = direccion;
-//         this.propietario = propietario;
-//         this.rubro = rubro
-//     }
-//     // Metodo para verificar si en X horario la tienda se encuentra abierta o cerrada;
-//     estaAbierto(hora){
-//         if((hora >= 9 && hora <= 12) || (hora >=15 && hora <= 19)){
-//             return true;
-//         }
-//         else{
-//             return false;
-//         }
-//     }
-//     // Metodo para verificar si X nombre es propietario de alguna de las tiendas;
-//     esPropietario(nombre){
-//         return nombre == this.propietario;
-//     }
-// }
-
-// const tienda1 = new Tienda("Delivery Pet Food", "Almafuerte 3124", "EMILIANO MARTINEZ", "Alimento para Mascotas");
-// const tienda2 = new Tienda("Viajes.net", "Leon de Iraeta 979", "MANUELA GODIO", "Viajes");
-// const tienda3 = new Tienda("Pavitas", "asdasda", "asdasd", "adsasdasd");
-
-
-// for(let index = 0; index < 5; index++){
-//     let entrada = prompt("Ingresar nombre propietario.");
-//     if(tienda1.esPropietario(entrada)){
-//         alert(`${tienda1.propietario} es propietario de ${tienda1.nombre}!`);
-//     }
-//     if(tienda2.esPropietario(entrada)){
-//         alert(`${tienda2.propietario} es propietario de ${tienda2.nombre}!`);
-//     }
-//     if(tienda3.esPropietario(entrada)){
-//         alert(`${tienda3.propietario} es propietario de ${tienda3.nombre}!`);   
-//     }
-// }
-
-const numeros = [1, 2, 3, 4, 5, 6, 7];
-console.log(numeros.toString());
-
-// Utilizando objeto Math y sus metodos
-const menorAMayor = (arr)=>{
-    console.log(arr.sort((a, b)=> b - a));
-}
-menorAMayor(numeros);
-
-let randomNum = 0;
-for (let index = 0; index < 10; index++) {
-    randomNum = Math.random();
-    console.log(randomNum);
-}
-const fecha = new Date();
-console.log(fecha);
-// El metodo .getDate() devuelve el dia de la fecha actual del ordenador
-console.log(fecha.getDate());
-// El metodo .getFullYear() devuelve el año completo de la fecha actual del ordenador
-console.log(fecha.getFullYear());
-// El metodo .getDay() devuelve el dia de la semana
-console.log(fecha.getDay());
-// Se muestra hora utilizando dos metodos del objeto Date
-console.log(`La hora actual es: ${fecha.getHours()}:${fecha.getMinutes()}hs`);
-btnMostrar.addEventListener("click", (e)=>{
-    e.preventDefault();
-    saludar("Alan");
-});
-
-const saludar = (nombre)=>{
-    textInput.value = `Hola ${nombre}`;
-}
-
-const arr = [1,2,3,4,5];
-
-const newArr = arr.map(n => n * 2);
-console.log(`Arr: ${arr}`);
-console.log(`newArr: ${newArr}`);
-console.log(arr.toString());
-console.log(arr.join(" - "));
-
-const destinos = [
-    {
-        nombre: "Brasil",
-        precio: 130000,
-        disponible: true,
-        img: "../assets/img/rio-de-janeiro.jpg"
-    },
-    {
-        nombre: "La Havana",
-        precio: 90000,
-        disponible: false,
-        img: "../assets/img/havana.jpg"
-    }
-];
-// El metodo forEach() aplica una funcion a cada elemento del array;
-destinos.forEach(destino => {
-    const imagenDestino = document.createElement("img");
-    imagenDestino.src= destino.img;
-    imagenDestino.classList.add("img-fluid");
-    imagenDestino.style.marginTop = "1rem";
-    document.body.append(imagenDestino);
-});
