cf3cffbeb208d98972c9c493d27f3db3a276e337
Alan Martinez <alannmartinezz@icloud.com>
2022-03-05 08:19:05 -0300

    problems solved

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 const successfullAddedAlert = document.querySelector(".alert-success");
+const tripRomvedAlert = document.querySelector(".alert-danger");
 const cartIconContainer = document.createElement("div");
 const modalCart = document.getElementById("modal-cart");
 
@@ -40,7 +41,7 @@ function addToCart(id) {
     let repetido = cart.find(search => search.id == id);
     if(repetido){
         repetido.count = repetido.count + 1;
-        document.getElementById(`cantidad${repetido.id}`).textContent = `Pasajes: ${repetido.count}`;
+        document.getElementById(`cantidad${repetido.id}`).innerHTML = `<i class="far fa-user"></i>Pasajes: ${repetido.count}`;
     }else{
         const tripToAdd = trips.find(trip => trip.id == id);
         // Add a count value
@@ -62,30 +63,39 @@ function showCart(tripToAdd) {
     let div = document.createElement("div");
     div.className = "trip";
     div.innerHTML = `<p class="trip-destination">${tripToAdd.destination}</p>
-    <p class="trip-price">Precio:  ${tripToAdd.price}</p>
-    <p id="cantidad${tripToAdd.id}">Pasajes: ${tripToAdd.count}</p>
-    <i class="fas fa-trash" id="btnRemoveTrip"></i>`;
+    <p class="trip-price">Precio:  $${tripToAdd.price}</p>
+    <p id="cantidad${tripToAdd.id}"><i class="far fa-user"></i>Pasajes: ${tripToAdd.count}</p>
+    <i class="fas fa-trash" id="btnRemoveTrip${tripToAdd.id}"></i>`;
     modalCartContent.appendChild(div);
 
-    const btnRemoveTrip = document.querySelectorAll("#btnRemoveTrip");
-    btnRemoveTrip.forEach(btn => {
-        btn.addEventListener("click", ()=>{
+    // let totalPrice = 0;
+    // totalPrice = tripToAdd.price;
+    // let p = document.createElement("p");
+    // p.className = "total-price";
+    // p.textContent = `Precio Total: $${totalPrice}`
+    // modalCart.appendChild(p);
+
+    const btnRemoveTrip = document.getElementById(`btnRemoveTrip${tripToAdd.id}`);
+    btnRemoveTrip.addEventListener("click", ()=>{
             if(tripToAdd.count == 1){
                 // Remove the father element of the trash icon when count is below 1 
-                btn.parentElement.remove();
+                btnRemoveTrip.parentElement.remove();
+                console.log(tripToAdd.id);
                 cart = cart.filter(trip => trip.id != tripToAdd.id);
                 // Update Cart
                 saveInLS("cart", cart);
                 // Update Cart Icon
                 showCartIcon();
+                removedAlert();
             }else{
                 // Reduce the count in 1
+                console.log(tripToAdd.id);
                 tripToAdd.count = tripToAdd.count - 1;
-                document.getElementById(`cantidad${tripToAdd.id}`).textContent = `Pasajes: ${tripToAdd.count}`;
+                document.getElementById(`cantidad${tripToAdd.id}`).innerHTML = `<i class="far fa-user"></i>Pasajes: ${tripToAdd.count}`;
+                removedAlert();
             }
         })
-    })
-}
+    }
 
 // Function Save in Local Storage
 
@@ -109,12 +119,22 @@ function showCartIcon (){
 
 showCartIcon();
 
-// Alert successfull added 
+// Alert Trip Successfully Added 
+
 function showAlert() {
     successfullAddedAlert.classList.add("fade-in-top");
     setTimeout(()=>{ 
         successfullAddedAlert.classList.remove("fade-in-top")
-    },2500);
+    },1500);
+}
+
+// Alert Trip Removed
+
+function removedAlert() {
+    tripRomvedAlert.classList.add("fade-in-top");
+    setTimeout(()=>{ 
+        tripRomvedAlert.classList.remove("fade-in-top")
+    },1500);
 }
 
 const btnCloseModalCart = document.getElementById("btnCloseModalCart");
---
+++ js/search.js
@@ -0,0 +1,15 @@
+const searchInput = document.getElementById("searchInput");
+const searchResult = document.getElementById("searchResult");
+
+
+searchInput.addEventListener("input", ()=>{
+    if(searchInput.value.length > 0){
+        trips.forEach( trip => {
+            let tripDestination = trip.destination;
+            if(tripDestination.toLowerCase().includes(searchInput.value.toLowerCase())){
+                searchResult.innerHTML = "";
+                searchResult.innerHTML += `<p>${tripDestination} ${trip.price}</p>`;
+            }
+        })
+    }
+})
