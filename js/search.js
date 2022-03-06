001c970f1c537889c9641b2ae1df208db9996cf5
Alan Martinez <alannmartinezz@icloud.com>
2022-03-05 22:01:22 -0300

    updating footer style

--- js/search.js
+++ js/search.js
@@ -2,14 +2,14 @@ const searchInput = document.getElementById("searchInput");
 const searchResult = document.getElementById("searchResult");
 
 
-searchInput.addEventListener("keyup", ()=>{
-    if(searchInput.value.length > 0){
-        trips.forEach( trip => {
-            let tripDestination = trip.destination;
-            if(tripDestination.toLowerCase().includes(searchInput.value.toLowerCase())){
-                searchResult.innerHTML = "";
-                searchResult.innerHTML += `<p>${tripDestination} ${trip.price}</p>`;
-            }
-        })
-    }
-})
+// searchInput.addEventListener("keyup", ()=>{
+//     if(searchInput.value.length > 0){
+//         trips.forEach( trip => {
+//             let tripDestination = trip.destination;
+//             if(tripDestination.toLowerCase().includes(searchInput.value.toLowerCase())){
+//                 searchResult.innerHTML = "";
+//                 searchResult.innerHTML += `<p>${tripDestination} ${trip.price}</p>`;
+//             }
+//         })
+//     }
+// })
